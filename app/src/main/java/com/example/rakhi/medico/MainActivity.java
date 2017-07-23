6120322bf2f6299061d41c04660c032595e0e64c
rakhi <darshirakhi@gmail.com>
2017-07-23 15:01:18 +0530

    First commit

---
+++ app/src/main/java/com/example/rakhi/medico/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.rakhi.medico;
+
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+    CollapsingToolbarLayout collapsingToolbarLayout=null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        collapsingToolbarLayout=(CollapsingToolbarLayout)findViewById(R.id.collapsing_toolbar);
+        collapsingToolbarLayout.setTitle("Collapsing");
+    }
+}
